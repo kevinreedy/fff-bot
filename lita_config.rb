90c7ca1c50f6e4f8057b3ae5133b9ba25049a509
Kevin Reedy <kevinreedy@gmail.com>
2017-09-06 02:45:12 -0500

    Fix redis config deprecation

--- lita_config.rb
+++ lita_config.rb
@@ -12,8 +12,8 @@ Lita.configure do |config|
   config.robot.adapter = :slack
   config.adapters.slack.token = ENV['SLACK_TOKEN']
 
-  config.redis.host = ENV['REDIS_HOST'] || '127.0.0.1'
-  config.redis.port = ENV['REDIS_PORT'] || 6379
+  config.redis[:host] = ENV['REDIS_HOST'] || '127.0.0.1'
+  config.redis[:port] = ENV['REDIS_PORT'] || 6379
 
   config.handlers.espn_fantasy_football.league_id = ENV['ESPN_LEAGUE_ID']
   config.handlers.espn_fantasy_football.season_id = ENV['ESPN_SEASON_ID'] || '2017'
